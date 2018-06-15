9c03f0abe6cbad047cde590d6f393f2a56fb132a
Niko <n.x.peralta@gmail.com>
2018-06-15 17:02:09 +0000

    Game is pretty much finished. Will look for bugs

--- buttonGame.py
+++ buttonGame.py
@@ -5,9 +5,11 @@ import RPi.GPIO as GPIO
 PIN_R = 17
 PIN_B = 27
 PIN_G = 22
+PINS = [PIN_R, PIN_B, PIN_G]
 LED_R = 13
 LED_B = 19
 LED_G = 26
+LEDS = [LED_R, LED_B, LED_G]
 START_TIME = time.time()
 LAST_PUSH = {LED_R:START_TIME, LED_B:START_TIME, LED_G:START_TIME}
 
@@ -17,17 +19,14 @@ LAST_PUSH = {LED_R:START_TIME, LED_B:START_TIME, LED_G:START_TIME}
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False) # gets rid of stupid channel in use warnings
 # Setting up button pins as inputs
-GPIO.setup(PIN_R, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(PIN_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(PIN_G, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for x in PINS:
+    GPIO.setup(x, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # Associate LED pins as outputs
-GPIO.setup(LED_R, GPIO.OUT)
-GPIO.setup(LED_B, GPIO.OUT)
-GPIO.setup(LED_G, GPIO.OUT)
+for x in LEDS:
+    GPIO.setup(x, GPIO.OUT)
 # Add event listeners
-GPIO.add_event_detect(PIN_R,GPIO.FALLING)
-GPIO.add_event_detect(PIN_B,GPIO.FALLING)
-GPIO.add_event_detect(PIN_G,GPIO.FALLING)
+for x in PINS:
+    GPIO.add_event_detect(x,GPIO.FALLING)
 
 # Keeps LED on while PIN is pushed down
 def lightUpPress (PIN, LED):
@@ -45,17 +44,51 @@ def lightUpPress (PIN, LED):
         GPIO.output(LED, GPIO.LOW)
     return pushed
 
-# Main 
-def main():
-    while True:
+# Checks stuff
+def isMatch(progress, complete):
+    for x in range(len(progress)):
+        if progress[x] != complete[x]:
+            return False
+    return True
+# Input phase
+def Player(match):
+    pushed = []
+    while len(pushed) < len(match) and isMatch(pushed, match):
 #        print(time.time()-LAST_PUSH[LED_R])
         if time.time()-LAST_PUSH[LED_R] > 0.25 and lightUpPress(PIN_R, LED_R):
-            print("RED")
+            pushed.append('R') #print("RED")
         if time.time()-LAST_PUSH[LED_B] > 0.25 and lightUpPress(PIN_B, LED_B):
-            print("BLUE")
+            pushed.append('B') #print("BLUE")
         if time.time()-LAST_PUSH[LED_G] > 0.25 and lightUpPress(PIN_G, LED_G):
-            print("GREEN")
-        # Quits upon simultaneous 3-button press
-        if (GPIO.input(PIN_R)==GPIO.input(PIN_B)==GPIO.input(PIN_G)==GPIO.LOW):
-            break
+            pushed.append('G') #print("GREEN")
+        for x in LEDS:
+            GPIO.output(x,GPIO.LOW)
+    return pushed
+
+# Simon's turn
+def Simon(hist):
+    choices = ['R','B','G']
+    mappy = {'R':LEDS[0],'B':LEDS[1],'G':LEDS[2]}
+    temp = hist
+    randomNum = (int)((time.time()%1)*3)
+    temp.append(choices[randomNum])
+    for x in temp:
+        GPIO.output(mappy[x],GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(mappy[x],GPIO.LOW)
+        time.sleep(0.5)
+    return temp
+
+def game():
+    simonSays = []
+    player = []
+    while isMatch(player,simonSays):
+        simonSays = Simon(simonSays)
+        player = Player(simonSays)
+        time.sleep(1)
+    print("GAME OVER")
+
+def main():
+    game()
+
 main()
